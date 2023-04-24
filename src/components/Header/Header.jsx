e030f006ee19625b2e2507d54ea8cd592ecef1d0
LaZy1220 <4elove4iska@gmail.com>
2023-04-24 12:20:24 +0300

    finish Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import Logo from "../../../public/logo.svg";
 import styles from "./Header.module.scss";
+import cn from "classnames";
 
 export const Header = () => {
+  const [activeButton, setActiveButton] = useState(1);
   return (
     <header className={styles.header}>
       <div className="container">
@@ -11,8 +14,22 @@ export const Header = () => {
             <span>Jobored</span>
           </div>
           <div className={styles.buttons}>
-            <span className={styles.active}>Поиск Вакансий</span>
-            <span>Избранное</span>
+            <span
+              onClick={() => setActiveButton(1)}
+              className={cn(styles.button, {
+                [styles.active]: activeButton === 1,
+              })}
+            >
+              Поиск Вакансий
+            </span>
+            <span
+              onClick={() => setActiveButton(2)}
+              className={cn(styles.button, {
+                [styles.active]: activeButton === 2,
+              })}
+            >
+              Избранное
+            </span>
           </div>
         </div>
       </div>
