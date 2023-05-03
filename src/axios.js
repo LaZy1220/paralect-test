798487c081b794fbe13c330150827d551f009bf8
LaZy1220 <4elove4iska@gmail.com>
2023-05-03 12:56:10 +0300

    push

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const apiKey = import.meta.env.VITE_APP_API_KEY;
+const apiKey = "GEU4nvd3rej*jeh.eqp";
 const apiUrl = "https://startup-summer-2023-proxy.onrender.com/2.0/";
-const apiClientSecret = import.meta.env.VITE_APP_API_CLIENT_SECRET;
+const apiClientSecret =
+  "v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948";
 const accessToken = window.localStorage.getItem("access_token");
 const instance = axios.create({
   baseURL: apiUrl,
--- src/services/getToken.js
+++ src/services/getToken.js
@@ -1,10 +1,11 @@
 import axios from "../axios";
 
-const apiLogin = import.meta.env.VITE_APP_API_LOGIN;
-const apiPassword = import.meta.env.VITE_APP_API_PASSWORD;
-const apiClientId = import.meta.env.VITE_APP_API_CLIENT_ID;
-const apiClientSecret = import.meta.env.VITE_APP_API_CLIENT_SECRET;
-const apiHr = import.meta.env.VITE_APP_API_HR;
+const apiLogin = "sergei.stralenia@gmail.com";
+const apiPassword = "paralect123";
+const apiClientId = "2356";
+const apiClientSecret =
+  "v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948";
+const apiHr = "0";
 
 export const getToken = async () => {
   try {
